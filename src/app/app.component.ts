2f9f8735371f7ffe2f6f5d485d027b5bb93ab7de
krow <ehvalero23@gmail.com>
2024-02-08 01:33:00 -0600

    Se agrega la funcionalidad de Translate,about,routerlink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {TranslateModule, TranslateLoader, TranslateService} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent]
+    imports:  [
+              RouterOutlet,
+              HeaderComponent,
+              HttpClientModule,
+              TranslateModule
+             ],
 })
 export class AppComponent {
   title = 'krowFotografia';
+  translateService = inject(TranslateService);
+
+  onSelectChange(event: any) {
+    console.log(event.target.value);
+    this.translateService.use(event.target.value);
+  }
 }
